c8b4dba85bb7772e404a108eb871134596cb8496
Duncan Abraham <duncan.defra@gmail.com>
2019-09-26 15:11:27 +0100

    added journey to the root page to fix the "protected area" link

--- demo/routes/root.js
+++ demo/routes/root.js
@@ -13,6 +13,7 @@ module.exports = [
         title: 'home',
         user: null,
         idm,
+        journey: 'chemicals',
         serviceLookup,
         claims: await idm.getClaims(request)
       })
