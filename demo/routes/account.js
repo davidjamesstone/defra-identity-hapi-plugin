932effeb46ae7654046626d8202b72316b770a55
Duncan Abraham <duncan.defra@gmail.com>
2019-10-02 08:48:53 +0100

    Added a comment to explain a null key being added to the accountNameFromId lookup

--- demo/routes/account.js
+++ demo/routes/account.js
@@ -35,7 +35,7 @@ module.exports = [
       const accountIds = rawConnections.map(conn => conn.accountId)
       const accounts = await idm.dynamics.readAccounts(accountIds)
       const accountNameFromId = accounts.reduce((acc, a) => ({ ...acc, [a.accountId]: a.accountName }), {})
-      Object.assign(accountNameFromId, { null: 'Citizen' })
+      Object.assign(accountNameFromId, { null: 'Citizen' }) // Add a key for "null" which returns "Citizen"
       let services = []
       // create data structures for the view inluding placeholder rows for accounts for which we don't have an enrolment
       rawConnections.forEach((conn) => {
