332f82a929343f89066e7c0a5b855bd9979bda34
Duncan Abraham <duncan.defra@gmail.com>
2019-10-07 16:07:23 +0100

    adding request handler and tweaking templates

--- demo/routes/request.js
+++ demo/routes/request.js
@@ -1,4 +1,5 @@
 const serviceLookup = require('../lib/services')
+
 /**
  * GUID
  * @typedef {string} GUID A globally unique identifier
@@ -11,24 +12,55 @@ module.exports = [
     options: {
       auth: 'idm'
     },
-    handler: async function (request, h) {
+    handler: async (request, h) => {
       const { journey } = request.params
       const { idm } = request.server.methods
-      const { serviceId, serviceName } = serviceLookup[journey]
-      const config = idm.getConfig()
-      config.serviceId = serviceId
-      await idm.refreshToken(request) // ensure we read the latest changes so the view reflects the database
       const claims = await idm.getClaims(request)
-      const { contactId } = claims || request.params
+      if (!claims) {
+        return h.redirect('/error')
+      }
+      const { contactId } = claims
+      // Get all unspent EnrolmentRequests
+      const currentEnrolmentRequests = await idm.dynamics.readEnrolmentRequests(null, contactId, false)
+      // convert the enrolments into the data structure required by the data rows in the view
+
+      const currentEnrolments = await idm.dynamics.readEnrolment(contactId, null, null, null, null, true)
+      const rawEnrolments = currentEnrolments.value || []
+      // convert the enrolments into the data structure required by the data rows in the view
+      const enrolments = rawEnrolments.map((thisEnrolment) => {
+        return {
+          accountName: thisEnrolment['_defra_organisation_value@OData.Community.Display.V1.FormattedValue'],
+          accountId: thisEnrolment['_defra_organisation_value'],
+          enrolmentType: thisEnrolment['_defra_servicerole_value@OData.Community.Display.V1.FormattedValue'],
+          serviceName: thisEnrolment['_defra_service_value@OData.Community.Display.V1.FormattedValue'],
+          serviceId: thisEnrolment['_defra_organisation_value'],
+          status: thisEnrolment['defra_enrolmentstatus@OData.Community.Display.V1.FormattedValue'],
+          statusId: thisEnrolment['defra_enrolmentstatus'],
+          roleName: thisEnrolment['_defra_serviceuser_value@OData.Community.Display.V1.FormattedValue'],
+          roleId: thisEnrolment['_defra_serviceuser_value'],
+          connectionDetailsId: thisEnrolment['_defra_connectiondetail_value']
+        }
+      })
+      // Return a list of services as {unid, accountName, enrolmentType, status}
+
       const enrolmentRequests = await idm.dynamics.readEnrolmentRequests(null, contactId, false)
+      const connectionIds = enrolmentRequests.map(r => r.connectionDetailsId)
+      const services = enrolments.filter(e => connectionIds.indexOf(e.connectionDetailsId) > -1)
+      services.sort((a, b) => {
+        return a.status + (a.accountName + '').toUpperCase() >= b.status + (b.accountName + '').toUpperCase() ? 1 : -1
+      })
       return h.view('request', {
-        ...request.payload,
-        ...request.params,
+        title: 'Requests',
+        idm,
+        journeyName: serviceLookup[journey].serviceName,
+        journey,
+        services,
+        enrolments: currentEnrolmentRequests.length,
         errorMessage: '',
-        enrolmentRequests,
-        serviceName,
         serviceLookup,
-        title: 'Requests'
+        claims: await idm.getClaims(request),
+        credentials: await idm.getCredentials(request),
+        trulyPrivate: false
       })
     }
   },
