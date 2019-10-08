5f840bd3323290d118bee353d3900f80dcd41603
Duncan Abraham <duncan.defra@gmail.com>
2019-10-08 13:23:12 +0100

    removed redundant code ...

--- demo/routes/status.js
+++ demo/routes/status.js
@@ -14,32 +14,29 @@ module.exports = [
       auth: 'idm'
     },
     handler: async (request, h) => {
-      const { journey } = request.params
       const { idm } = request.server.methods
       const claims = await idm.getClaims(request)
       if (!claims) {
         return h.redirect('/error')
       }
       const { contactId } = claims
+      const { journey } = request.params
 
       const currentEnrolments = await idm.dynamics.readEnrolment(contactId, null, null, null, null, true)
-      const rawEnrolments = currentEnrolments.value || []
       // convert the enrolments into the data structure required by the data rows in the view
-      const enrolments = rawEnrolments
+      const filteredEnrolments = currentEnrolments.value
         .map(thisEnrolment => new EnrolmentStatus(thisEnrolment))
         .filter(e => !e.isIdentity())
-
-      enrolments.sort((a, b) => { // Sort by organisation / status
-        return (a.accountName + '').toUpperCase() + a.status >= (b.accountName + '').toUpperCase() + b.status ? 1 : -1
-      })
+        .sort((a, b) => { // Sort by organisation / status
+          return (a.accountName + '').toUpperCase() + a.status >= (b.accountName + '').toUpperCase() + b.status ? 1 : -1
+        })
 
       return h.view('status', {
         title: 'Requests',
         idm,
         journeyName: serviceLookup[journey].serviceName,
         journey,
-        enrolments,
-        errorMessage: '',
+        filteredEnrolments,
         serviceLookup,
         claims: await idm.getClaims(request),
         credentials: await idm.getCredentials(request),
